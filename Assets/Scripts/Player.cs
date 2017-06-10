ea07a3784d5dc21854f6b636dc286c7594755da1
Henrik Hasell <henrikhasell@gmail.com>
2017-06-10 12:01:32 +0100

    Initial commit.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private float speed;
+
+	void Start () {
+		speed = 3f;
+	}
+
+	void Update () {
+		Vector2 desiredVelocity = new Vector2 (
+			Input.GetAxisRaw ("Horizontal"),
+			Input.GetAxisRaw ("Vertical")
+		);
+
+		desiredVelocity *= speed;
+
+		Rigidbody2D rigidBody2D = this.GetComponent<Rigidbody2D> ();
+		rigidBody2D.velocity = desiredVelocity;
+	}
+}
