40b4f43b760a07a7f23bcbf771ad13e16bf4fbc1
Sondre Agledahl <sondre.milch@gmail.com>
2017-06-11 00:58:21 +0100

    Sprite switch thingy.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private static MusicPlayer instance;
+    private AudioSource lightPlayer, darkPlayer;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        darkPlayer = transform.GetChild(0).GetComponent<AudioSource>();
+        lightPlayer = transform.GetChild(1).GetComponent<AudioSource>();
+        lightPlayer.mute = true;
+    }
+
+    private void handlePowerOff() {
+        lightPlayer.mute = true;
+        darkPlayer.mute = false;
+    }
+
+    private void handlePowerOn() {
+        darkPlayer.mute = true;
+        lightPlayer.mute = false;
+    }
+}
---
+++ Assets/SwitchSprite.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSprite : MonoBehaviour {
+
+    public Sprite lightSprite, darkSprite;
+    private SpriteRenderer mRend;
+
+	void Start () {
+        mRend = GetComponent<SpriteRenderer>();
+	}
+
+    private void handlePowerOff() {
+        mRend.sprite = darkSprite;
+    }
+
+    private void handlePowerOn() {
+        mRend.sprite = lightSprite;
+    }
+}
